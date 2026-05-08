0ac74b7bde1af005ab8385cf4cec0ac239e090b0
Tyler Williams <tyler.williams162@gmail.com>
2026-05-08 13:35:56 -0700

    Updated viz.py with make_fig function so all plots can be done at once, updated each plot function accordingly. Created 02_synth_verification notebook verifying the plots for each signal type.

--- dsp/viz.py
+++ dsp/viz.py
@@ -1,26 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from datasets import SyntheticData
 from typing import Optional
 from scipy.fft import fft, fftshift, fftfreq
 from scipy.signal import spectrogram
-from synth import (generate_bpsk, generate_qpsk, generate_8psk,
-                   generate_qam, generate_fm, generate_fsk)
 
 
 
 
 
 def plot_time(iq, fs: int = 200e3,
-              title: Optional[str] = None):
+              title: Optional[str] = None,
+              n_samples: Optional[int] = None,
+              I_ax=None,
+              Q_ax=None):
+    '''
+    Plots the time domain of the signal as two plots I and Q.
+    
+    n_samples: Number of samples to be plotted
+    I_ax: the I axis, will be created if not passed in the argument
+    Q_ax: Q axis
+    '''
 
-    I = iq[0]
-    Q = iq[1]
+    I = iq.real
+    Q = iq.imag
 
     N = len(I)
     t = np.arange(N) / fs * 1e6
     
-    fig, (I_ax, Q_ax) = plt.subplots(2,1, figsize=(12,5))
+    if I_ax is None or Q_ax is None:
+        _, (I_ax, Q_ax) = plt.subplots(2,1, figsize=(12,5), sharex=True)
+
+
     I_ax.set_ylabel('I')
     Q_ax.set_ylabel('Q')
     Q_ax.set_xlabel(r'Time ($\mu$s)')
@@ -31,13 +41,13 @@ def plot_time(iq, fs: int = 200e3,
     I_ax.plot(t, I)
     Q_ax.plot(t, Q)
 
-    plt.tight_layout()
-    plt.show()
+    return I_ax, Q_ax
 
 def plot_fft(iq, fs: int=200e3,
-             title: Optional[str] = None):
-    I = iq[0]
-    Q = iq[1]
+             title: Optional[str] = None,
+             ax=None):
+    I = iq.real
+    Q = iq.imag
 
     signal = I + 1j * Q
 
@@ -47,7 +57,9 @@ def plot_fft(iq, fs: int=200e3,
     n = I.size
     f = fftshift(fftfreq(n, d=1/fs))
 
-    fig, ax = plt.subplots(1,1)
+    if ax is None:
+        _, ax = plt.subplots(1,1)
+
     ax.plot(f/1e3, power_db)
     ax.set_xlabel('Frequency (kHz)')
     ax.set_ylabel('Power (dB)')
@@ -55,21 +67,24 @@ def plot_fft(iq, fs: int=200e3,
     if title is not None:
         ax.set_title(title)
 
-    plt.tight_layout()
-    plt.show()
+    return ax
+    
 
 
 def plot_spectrogram(iq, fs: int=200e3,
-                     title: Optional[str]=None):
-    I = iq[0]
-    Q = iq[1]
+                     title: Optional[str]=None,
+                     ax=None):
+    I = iq.real
+    Q = iq.imag
 
     IQ = I + 1j *  Q
 
-    f, t, Sxx = spectrogram(IQ, fs=fs, nperseg=64)
+    f, t, Sxx = spectrogram(IQ, fs=fs, nperseg=64, return_onesided=False)
     Sxx_db    = 10 * np.log10(np.abs(Sxx) + 1e-10)
 
-    fig, ax = plt.subplots(figsize=(10, 4))
+    if ax is None:
+        _, ax = plt.subplots(figsize=(10, 4))
+
     mesh = ax.pcolormesh(t * 1e3, f / 1e3, Sxx_db,
                          shading='gouraud', cmap='inferno')
     plt.colorbar(mesh, ax=ax, label='Power (dB)')
@@ -77,14 +92,15 @@ def plot_spectrogram(iq, fs: int=200e3,
     ax.set_ylabel('Frequency (kHz)', fontsize=11)
     ax.set_ylim(-100, 0)
     ax.set_title(title or 'Spectrogram', fontsize=13)
-    plt.tight_layout()
-    plt.show()
+
+    return ax
     
 
-def plot_constellations(iq, title: Optional[str] = None):
+def plot_constellations(iq, title: Optional[str] = None,
+                        ax=None):
 
-    
-    fig, ax = plt.subplots(1,1)
+    if ax is None:
+        _, ax = plt.subplots(1,1)
     if title is not None:
         ax.set_title(title)
 
@@ -94,16 +110,45 @@ def plot_constellations(iq, title: Optional[str] = None):
     ax.axvline(0)
 
 
-    ax.scatter(iq[0], iq[1])
+    ax.scatter(iq.real, iq.imag)
+    
+    return ax
+
+
+def make_fig(signals: dict, 
+             plot_fn,
+             title: Optional[str] = None,
+             **plot_kwargs):
+    '''
+    Creates a figure showing the plots for all passed signal types for comparison, 
+    can be used to create stand alone plots.
+
+    signals: A dictionary with each signal type ("BPSK", "QPSK", etc) as the key,
+             and the generated iq as  the value.
+    
+    plot_fn: The type of plot the user wishes to create (plot_time, plot_fft, etc.
+
+    title: Title for the overall figure
+    '''
+    n = len(signals)
+    #plot_time needs a separate figure creation to deal with the separate I and Q axes.
+    if plot_fn == plot_time:
+        fig, axes = plt.subplots(2, n, figsize=(5 * n, 6))
+        for i, (name, iq) in enumerate(signals.items()):
+            plot_time(iq, I_ax=axes[0, i], Q_ax=axes[1, i],
+                             title=name, **plot_kwargs)
+    #figure creation for all other plot types
+    else:
+        fig,axes = plt.subplots(1, n, figsize=(5*n, 4))
+        if n == 1:
+            axes = [axes]
+        for ax, (name, iq) in zip(axes, signals.items()):
+            plot_fn(iq, ax=ax, title=name, **plot_kwargs)
+
+    if title:
+        fig.suptitle(title, fontsize=14, fontweight="bold")
+
+    plt.tight_layout()
     plt.show()
 
 
-
-sig = generate_qpsk(32, snr_db=20.0)
-iq = ([sig.real, sig.imag])
-
-
-#plot_fft(iq, title="Synthetic BPSK PSD")
-#plot_time(iq, title='Synthetic IQ Time Domain')
-#plot_constellations(iq)
-plot_spectrogram(iq)
